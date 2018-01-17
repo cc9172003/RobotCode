6181904a822361160ce5b73da246a6398f12e776
Lucas <plutonjeong@gmail.com>
2018-01-16 16:09:39 -0800

    1/16/2018

--- src/org/usfirst/frc/team3952/robot/Robot.java
+++ src/org/usfirst/frc/team3952/robot/Robot.java
@@ -1,142 +1,53 @@
 package org.usfirst.frc.team3952.robot;
 
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.drive.*;
 
-/**
- * 0 = back right
-   2 = nothing
-	3 = climber
-		//4 = front right
-		//5 = nothing
-		//6 = nothing
-		//7 = agitator
-		//8 = launcher motor
-		//9 = left front
-* 
-* Rules which will keep this code easy to read
-* 
-* 0. Put decision logic (when should we shoot, when should we agitate) in Controller, put motor and 
-* camera actions in Systems (MechanumWheels class, Shooter class) and keep the Robot class 
-* as slim as possible. The less code there is in Robot, the easier it is to read.
-* 1. Try to not pass controller in as an argument in method calls. That way,
-* all the controller decision making is inside Robot. Itll make it easier to
-* find out why certain things do what they do. If you pass in controller, you will have to
-* look into that method, find out what it does and repeat for all other method calls 
-* which accept controller as an argument. Note, this is just a part of Rule 0.
-* 2. Keep Joystick, XboxController and others inside Controller class. This makes it easy to switch
-* the actual controller without having to dig through a lot of code. If you want to switch controllers
-* all you gotta do is change Controller class.
-* 3. Prefix Controller boolean methods with should. For example
-* 	public boolean shouldShoot(){
-* 		//figures out whether it should shoot based on joy stick or xbox or something.
-*   }
-* 
-* 
- */
 public class Robot extends IterativeRobot {
-	private Shooter shooter;
-	private MechanumWheels mechWheels;
+	private static final int LEFT = 3;
+	private static final int RIGHT = 4;
+	private static final double ROTATION_SPEED = 0.2;
+	private static final double LERP = 0.1;
 	
-	private Controller controller;
-	private AnalogUltrasonic analogUltrasonic;
-	//private UsbCamera camera;
+	private Talon frontRight, frontLeft, rearRight, rearLeft;
+	private Joystick joystick;
+	private MecanumDrive drive;
+
+	private double xVel;		//x velocity
+	private double yVel;		//y velocity
+	private double aVel;		//angular velocity
 	
-	private Task currentTask;
-	
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
 	@Override
 	public void robotInit() {
-		mechWheels = new MechanumWheels(
-				new Talon(9),
-				new Talon(4),
-				new Talon(1),
-				new Talon(0)
-		);
-		shooter = new Shooter(
-				new Talon(8),
-				new Talon(7)
-		);
-		controller = new Controller();
-		currentTask = new TelopTask(this);
-		analogUltrasonic = new AnalogUltrasonic(2);
+		frontRight = new Talon(0);	//TODO: change parameter to correct pin
+		frontLeft = new Talon(0);
+		rearRight = new Talon(0);
+		rearLeft = new Talon(0);
+		joystick = new Joystick(0);
+		drive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 		
-		//camera = CameraServer.getInstance().startAutomaticCapture();
-		//camera.setResolution(640, 480);
-		
+		xVel = 0;
+		yVel = 0;
+		aVel = 0;
 	}
-	
-
-	@Override
-	public void autonomousInit() {
-	}
-
-	@Override
-	public void autonomousPeriodic() {
-	}
-
 	
 	@Override
 	public void teleopPeriodic() {
-		if(controller.shouldStartLineUpToWall()){
-			currentTask.cancel();
-			currentTask = new LineUpTask(this);
-			
-		}
-
-		//should be last of controller checking.
-		if(controller.cancelTask()){
-			currentTask.cancel();
-			currentTask = new TelopTask(this);
-		} 
-
-		boolean done = currentTask.performTask();
-		if(done){
-			currentTask = new TelopTask(this);
-		}  
-		
-		
-		//smart dashboard stuffs
-		SmartDashboard.putNumber("Ultrasonic Distance", Math.round(analogUltrasonic.getDistance()));
-		SmartDashboard.putString("Current Task", currentTask.toString());
-		
+		xVel = lerpVel(xVel, joystick.getX());
+		yVel = lerpVel(yVel, joystick.getY());
+		aVel = lerpVel(aVel, getRotation());
+		drive.driveCartesian(yVel, xVel, aVel);
 	}
 	
-
-	
-	@Override
-	public void testPeriodic() {
-		
-	}
-
-	
-	/**
-	 * Getters
-	 */
-	public Shooter getShooter() {
-		return shooter;
-	}
-
-	public MechanumWheels getMechWheels() {
-		return mechWheels;
-	}
-
-	public Controller getController() {
-		return controller;
+	private double getRotation() {
+		double rotation = ROTATION_SPEED * ((joystick.getRawButton(RIGHT)) ? 1 : 0 - (joystick.getRawButton(LEFT) ? 1 : 0));
 	}
 	
-	public AnalogUltrasonic getAnalogUltrasonic(){
-		return analogUltrasonic;
+	private double lerpVel(double x, double destination) {
+		return constraint(x + (destination - x) * LERP, -1, 1);
 	}
 	
-	
+	private double constraint(double x, double min, double max) {
+		return Math.min(Math.max(x, min), max);
+	}
 }
-

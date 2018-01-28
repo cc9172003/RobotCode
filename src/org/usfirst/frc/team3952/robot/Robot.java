dbfd1df3e3f2c4020af2e09c184c0294db7584cf
DumboOctopus <neilp0101@gmail.com>
2018-01-27 16:21:29 -0800

    Autonomous Framework

--- src/org/usfirst/frc/team3952/robot/Robot.java
+++ src/org/usfirst/frc/team3952/robot/Robot.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team3952.robot;
 
 import edu.wpi.first.wpilibj.*;
+import java.util.*;
 import edu.wpi.first.wpilibj.drive.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
@@ -12,6 +13,8 @@ public class Robot extends IterativeRobot {
 	private Task currentTask, backgroundTask;
 	private Encoder rightEncoder, leftEncoder;
 	private ADXRS450_Gyro gyro;
+	
+	private Queue<Task> autonomousQueue;
 	
 	
 	@Override
@@ -26,6 +29,7 @@ public class Robot extends IterativeRobot {
 		rightEncoder = new Encoder(0, 1);
 		leftEncoder = new Encoder(2, 3);
 		gyro = new ADXRS450_Gyro();
+		autonomousQueue = new LinkedList<>();
 		//gyro.calibrate();
 		
 	}
@@ -66,6 +70,27 @@ public class Robot extends IterativeRobot {
 		
 		SmartDashboard.putString("Current Task", currentTask.toString());
 	}
+	
+	@Override
+	public void autonomousInit(){
+		String stuff = DriverStation.getInstance().getGameSpecificMessage(); //ex: LRL
+		
+	}
+	
+	@Override
+	public void autonomousPeriodic(){
+		if(!autonomousQueue.isEmpty()){
+			if(autonomousQueue.peek().run()){
+				autonomousQueue.poll();
+			}
+		}
+		
+	}
+	
+	
+	
+	
+	//=============================GETTERS=============================//
 	
 	public Controller getController() {
 		return controller;
